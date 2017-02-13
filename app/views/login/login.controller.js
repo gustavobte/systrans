e50f2123c32144cdca4ddac25db16fb73085b4dc
Gustavo Silva <gustavobte@gmail.com>
2017-02-13 18:45:48 -0200

    - Merge

---
+++ app/views/login/login.controller.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('stLogin')
+        .controller('LoginController', LoginController);
+
+    /* @ngInject */
+    function LoginController() {
+        var vm = this;
+
+
+    }
+
+})();
+
